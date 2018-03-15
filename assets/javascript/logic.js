cb4425dd1c84ef74e4cbe5fd449788d588603de7
rsarge47 <35145102+rsarge47@users.noreply.github.com>
2018-03-15 19:03:25 -0400

    Deleted old code that was in comment tags

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+    // Materialize Section
     $('.tooltipped').tooltip({
         delay: 50
     });
@@ -15,7 +15,8 @@ $(document).ready(function () {
         alignment: 'left', // Displays dropdown with edge aligned to the left of button
         stopPropagation: false // Stops event propagation
     });
-
+    //----------------------------------------------------------------------------
+    
     // Firebase config and initialization
     var config = {
         apiKey: "AIzaSyD1KKJPWXtJjBrQsi6oR6rgLVNw5ckKnOE",
@@ -26,26 +27,21 @@ $(document).ready(function () {
         messagingSenderId: "585051342056"
     };
     firebase.initializeApp(config);
+    //----------------------------------------------------------------------------
 
     var searchCounter = 0;
-
     // This takes form input and searches RecipeAPI, then displays recipes
     function displayRecipe() {
+        // Variables for creating queryURL
         var ingredient1 = $('#ingredient1').val().trim();
         var ingredient2 = $('#ingredient2').val().trim();
         var ingredient3 = $('#ingredient3').val().trim();
         var ingredients = ingredient1 + "+" + ingredient2 + "+" + ingredient3;
-        console.log("ingredients: " + ingredients);
         var search = "q=" + ingredients;
-
         var dietType = document.querySelector('option[name="diet"]:checked').value;
-        console.log("Diet: " + dietType);
-        diet = "&diet=" + dietType;
-
+            diet = "&diet=" + dietType;
         var healthType = document.querySelector('option[name="health"]:checked').value;
-        console.log("Health: " + healthType);
-        health = "&health=" + healthType;
-
+            health = "&health=" + healthType;
         var base = "https://api.edamam.com/search?";
         var appId = "&app_id=bcfc903e";
         var appKey = "&app_key=1878067c0d9fc8775b5834269ffb3bed";
@@ -53,9 +49,8 @@ $(document).ready(function () {
         var queryURL = base + search + appId + appKey + diet + health;
         console.log("queryURL: " + queryURL);
 
-        // Pushing search criteria to firebase
         searchCounter++;
-
+        // Pushing search criteria to firebase
         firebase.database().ref().push({
             searchCount: searchCounter,
             ing1: ingredient1,
@@ -75,12 +70,14 @@ $(document).ready(function () {
             var results = response.hits;
             var recipeDiv = $("<div class='displayRecipe'>");
 
+            // Checking for False Boolean on JSON search results
             if (!response.more) {
                 var noResponse = $("<h3 class='noResponse'>").text("Sorry, no recipes for " + ingredient1 + ", " + ingredient2 + ", " + ingredient3);
                 $("#recipeDisplay").prepend(noResponse);
                 console.log(noResponse);
             }
 
+            // Creating Recipe Display from JSON search results
             for (var i = 0; i < results.length; i++) {
                             
                 var recipeName = $("<h3 class='recipeName'>").text(results[i].recipe.label);            
@@ -97,7 +94,7 @@ $(document).ready(function () {
         });
     };
 
-    // When you click submit button a search is performed in the recipe API
+    // Seaerch recipe API button
     $('#submit').on('click', function (event) {
         event.preventDefault();
         displayRecipe();
@@ -127,11 +124,28 @@ $(document).ready(function () {
         snap.val().health + "</td></tr>");
     }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
-});
+    });
+    //----------------------------------------------------------------------------
+    
+    // Search Wiki API
 
-    /* function displayWiki() {
+    function displayWiki(){
         var wikiSearch = $('#wikiSearch').val().trim();
         console.log(wikiSearch);
+
+        function validateForm() {
+            if (wikiSearch == "") {
+                $("#alert").html("Search must be filled out");
+                return false;
+            } else {
+                $("#alert").html("");
+                return true;
+            }
+        }
+
+        if(!validateForm()) {
+            return;
+            }
 
         var wikiqueryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiSearch + "&limit=1&namespace=0&format=json";
         console.log(wikiqueryURL);
@@ -140,126 +154,14 @@ $(document).ready(function () {
             url: wikiqueryURL,
             method: "GET",
             dataType: 'jsonp'
-        }).then(function (response) {
+            }).then(function(response) {
             var results = response;
-
-            console.log(results);
-
             var wikiDiv = $("<div class='displayWiki'>");
             var wikiName = $("<h3 class='wikiName'>").text(results[1]);
             var wikiDescription = $("<p class='wikiDescription'>").text(results[2]);
             var wikiURL = $("<a class='wikiURL'>").text(results[3]).attr('href', results[3]).attr("target", "_blank");
-            $("#wikiDisplay").append(wikiName, wikiDescription, wikiURL);
-
+            $("#wikiDisplay").prepend(wikiName, wikiDescription, wikiURL);
         });
-
-    }
-
-    $('#wikiSubmit').on('click', function (event) {
-        event.preventDefault();
-        displayWiki();
-    });
-    */
-/*
-    
-    // Sends the queryURL to recipe API and returns JSON
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        dataType: "jsonp"
-        }).then(function(response) {        
-        var results = response.hits;
-        var recipeDiv = $("<div class='displayRecipe'>");
-
-        if (!response.more) {
-            var noResponse = $("<h3 class='noResponse'>").text("Sorry, no recipes for " + ingredient1 + ", " + ingredient2 + ", " + ingredient3);
-            $("#recipeDisplay").prepend(noResponse);
-            console.log(noResponse);
-        }
-
-        for (var i = 0; i < results.length; i++) {
-                        
-            var recipeName = $("<h3 class='recipeName'>").text(results[i].recipe.label);            
-            var recipeImage = $("<img class='recipeImage'>").attr("src", results[i].recipe.image).attr("alt", results[i].recipe.label);
-            var recipeIng = $("<p class='recipeIng'>").text(results[i].recipe.ingredientLines);
-            var recipeLink = $('<a class="recipeLink">').text(results[i].recipe.url).attr('href', results[i].recipe.url).attr("target", "_blank");
-
-            var infoDiv = $("<div class='recipe'>").append(recipeImage, recipeName, recipeIng, recipeLink);
-            recipeDiv.append(infoDiv);
-            
-        };
-        $("#recipeDisplay").prepend(recipeDiv);
-*/
-
-
-    /*
-    $("#wikiClear").on('click', function (event) {
-        event.preventDefault();
-        $("#wikiDisplay").empty();
-    });
-    */
-/*
-};
-
-// When you click submit button a search is performed in the recipe API
-$('#submit').on('click', function(event) {
-    event.preventDefault();
-    displayRecipe();
-    $('input[name="ingredient"]').val('');    
-});
-
-// Clear recipes button
-$('#clearRecipe').on('click', function(event) {
-    event.preventDefault();
-    $('#recipeDisplay').empty();
-})
-
-
-
-*/
-
-    //start of wiki API
-    //----------------------------------------------------------------------------
-
-
-    function displayWiki(){
-        var wikiSearch = $('#wikiSearch').val().trim();
-        console.log(wikiSearch);
-
-    function validateForm() {
-        if (wikiSearch == "") {
-            $("#alert").html("Search must be filled out");
-            return false;
-        } else {
-            $("#alert").html("");
-            return true;
-        }
-    }
-
-    if(!validateForm()) {
-        return;
-        }
-
-
-        var wikiqueryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + wikiSearch + "&limit=1&namespace=0&format=json";
-        console.log(wikiqueryURL);
-
-    $.ajax({
-        url: wikiqueryURL,
-        method: "GET",
-        dataType: 'jsonp'
-    }).then(function(response) {
-        var results = response;
-
-        console.log(results);
-
-        var wikiDiv = $("<div class='displayWiki'>");
-        var wikiName = $("<h3 class='wikiName'>").text(results[1]);
-        var wikiDescription = $("<p class='wikiDescription'>").text(results[2]);
-        var wikiURL = $("<a class='wikiURL'>").text(results[3]).attr('href', results[3]).attr("target", "_blank");
-        $("#wikiDisplay").prepend(wikiName, wikiDescription, wikiURL);
-
-    });
 
     }
 
